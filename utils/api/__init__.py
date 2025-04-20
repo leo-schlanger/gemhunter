6f97e00aa9f067f025a92e6be395a418508c1796
leo-schlanger <leoschlanger@gmail.com>
2025-04-20 14:21:48 -0300

    fix

--- utils/api/__init__.py
+++ utils/api/__init__.py
@@ -1,11 +1,13 @@
 from .gecko import fetch_token_stats_gecko
 from .geckoterminal import fetch_token_stats_terminal_by_address, fetch_token_stats_geckoterminal, fetch_token_from_geckoterminal_by_symbol
 from .dexscreener import search_tokens_dexscreener
+from .gecko import parse_float
 
 __all__ = [
     "fetch_token_stats_gecko",
     "fetch_token_stats_terminal_by_address",
     "fetch_token_stats_geckoterminal",
     "fetch_token_from_geckoterminal_by_symbol",
-    "search_tokens_dexscreener"
+    "search_tokens_dexscreener",
+    "parse_float"
 ]
